a2da19fc62048b55c70ac0c1a235d4c571a0685e
meet_hansalia <meethansalia99@gmail.com>
2023-07-24 17:30:24 +0530

    added authentication

--- auth/src/component/Auth/AuthForm.js
+++ auth/src/component/Auth/AuthForm.js
@@ -1,27 +1,78 @@
+import LoadingSpinner from '../UI/LoadingSpinner';
 import './AuthForm.css'
-import {useState} from 'react'
+import {useState, useRef } from 'react'
 
 const AuthForm = ()=>{
-   const[isLogin, setIsLogin] = useState(true)
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef()
+    const[isLogin, setIsLogin] = useState(true);
+    const [isLoading, setIsLoading]= useState(false);
+    
 
     const switchAuthModeHandler = () =>{
         setIsLogin((prevState) => !prevState);
     }
 
+    const submitHandler = (event) =>{
+        event.preventDefault();
+        const enteredEmail = emailInputRef.current.value;
+        const enteredPassword = passwordInputRef.current.value;
+
+        setIsLoading(true)
+
+        if(isLogin){
+            fetch(
+                'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]'
+            )
+        }else{
+            fetch(
+                'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAYevxC-o9PRiW0CKG3SSygjvBBWKWJZAo',
+                {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        email: enteredEmail,
+                        password: enteredPassword,
+                        returnSecureToken: true,
+                    }),
+                    headers: {  
+                        'Content-Type':'application/json'
+                    }
+                }
+            
+            ).then((res) =>{
+                setIsLoading(false);
+                if(res.ok){
+
+                }else{
+                    return res.json().then((data) =>{
+                        let errorMessage ='Authentication failed!';
+                        
+                        // if(data && data.error && data.error.message){
+                        //   errorMessage = data.error.message;
+                        // }
+
+                        alert(errorMessage);
+                    });
+                }
+            });
+        }
+    };
+
     return(
         <section className="auth">
             <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-            <form>
-                <div className="control">
+            <form onSubmit={submitHandler}>
+                <div className="controls">
                     <label htmlFor='email'>Your Email </label>
-                    <input type='email' id='email' required/>
+                    <input type='email' id='email' required ref={emailInputRef}/>
                 </div>
-                <div className="control">
+                <div className="controls">
                     <label htmlFor='password'>Your Password</label>
-                    <input type='password' id='password' required/>
+                    <input type='password' id='password' required ref={passwordInputRef}/>
                 </div>
-                <div className="action">
-                    <button>{isLogin ? 'Login' : 'Create Account'}</button>
+                <div className="actions">
+                   {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+                   {isLoading && <LoadingSpinner/>}
                     <button 
                         type='button'
                         className="toggle"
